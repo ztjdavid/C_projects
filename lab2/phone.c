7ec3e7a4a67960744ddc9012071a4a5606e5dfb4
Zhao Tingji <tingji.zhao@mail.utoronto.ca>
2021-01-22 00:14:10 -0500

    part 3 completed

---
+++ lab2/phone.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char phone[11];
+    int choice;
+
+    printf("Please enter the number:\n");
+    scanf("%s", phone);
+
+    printf("Please choose the digit:\n");
+    scanf("%d", &choice);
+
+    if(choice == -1){
+        printf("%s", phone);
+        return 0;
+    }else if(choice > -1 && choice < 10){
+        printf("%c", phone[choice]);
+        return 0;
+    }else{
+        printf("ERROR");
+        return 1;
+    }
+
+
+}
