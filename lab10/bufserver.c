66358c27db0d3406d0b9408aeecbd5e31389f895
Zhao Tingji <tingji.zhao@mail.utoronto.ca>
2021-03-25 15:39:10 -0400

    version 1.0

--- lab10/bufserver.c
+++ lab10/bufserver.c
@@ -38,7 +38,7 @@ int main() {
         int nbytes;
         while ((nbytes = read(fd, after, room)) > 0) {
             // Step 1: update inbuf (how many bytes were just added?)
-
+            inbuf = inbuf + nbytes;
 
             int where;
 
@@ -55,8 +55,7 @@ int main() {
                 // using print statement below.
                 // Be sure to put a '\0' in the correct place first;
                 // otherwise you'll get junk in the output.
-
-
+                buf[where-2] = '\0';
                 printf("Next message: %s\n", buf);
                 // Note that we could have also used write to avoid having to
                 // put the '\0' in the buffer. Try using write later!
@@ -67,10 +66,15 @@ int main() {
                 // You want to move the stuff after the full line to the beginning
                 // of the buffer.  A loop can do it, or you can use memmove.
                 // memmove(destination, source, number_of_bytes)
+                inbuf = inbuf - where;
+                memmove(&buf, &buf[where], inbuf);
+
 
 
             }
             // Step 5: update after and room, in preparation for the next read.
+            room = room - inbuf;
+            after = &buf[inbuf];
 
 
         }
@@ -91,5 +95,12 @@ int main() {
  * Definitely do not use strchr or other string functions to search here. (Why not?)
  */
 int find_network_newline(const char *buf, int n) {
+    for(int i = 0; i < n; i++){
+        if(buf[i] == '\r'){
+            if(buf[i+1] == '\n'){
+                return i+2;
+            }
+        }
+    }
     return -1;
 }
