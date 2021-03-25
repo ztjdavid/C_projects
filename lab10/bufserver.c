fec06b4d3d59534cb6c42242aa21cf6e31a4f77d
Zhao Tingji <tingji.zhao@mail.utoronto.ca>
2021-03-25 16:35:25 -0400

    version 1.1

--- lab10/bufserver.c
+++ lab10/bufserver.c
@@ -55,7 +55,7 @@ int main() {
                 // using print statement below.
                 // Be sure to put a '\0' in the correct place first;
                 // otherwise you'll get junk in the output.
-                buf[where-] = '\0';
+                buf[where-2] = '\0';
                 printf("Next message: %s\n", buf);
                 // Note that we could have also used write to avoid having to
                 // put the '\0' in the buffer. Try using write later!
